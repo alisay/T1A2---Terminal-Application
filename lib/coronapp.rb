b8cfa5156dd7b4aa98ae857b3d3ae8bc618d82fa
Alisa <alisa.blakeney@gmail.com>
2020-04-27 05:56:10 +1000

    :rocket: deploy scrape feature

--- lib/coronapp.rb
+++ lib/coronapp.rb
@@ -2,5 +2,21 @@ require "coronapp/version"
 
 module Coronapp
   class Error < StandardError; end
-  # Your code goes here...
+  
+  class Stat
+    def get_stat(given_stat)
+      stat_hash = {new: "new_daily_cases", deaths: "new_daily_deaths", total: "total_cases", recovered:"total_recoveries",toll: "total_deaths"}
+      return stat_hash[given_stat]
+    end
+  end  
+  
+  def self.get(options = {})
+      country = options.fetch(:country).upcase
+      date = options.fetch(:date)
+      stat = options.fetch(:stat).to_sym
+      url = "https://api.thevirustracker.com/free-api?countryTimeline=#{country}"
+      s = Stat.new
+      result = open(url).read
+      return "#{JSON.parse(result).dig("timelineitems",0, date, s.get_stat(stat))} #{s.get_stat(stat).gsub("_", " ")}"
+  end
 end
